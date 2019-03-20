f9b939e4749762264cc96c676b1b4d8640093c32
ascinitially <Blaise@Blaises-MacBook-Pro.local>
2019-03-20 16:18:39 -0400

    GIFtastic

--- assets/app.js
+++ assets/app.js
@@ -9,12 +9,11 @@ var butt;
 
 createButtons();
 
-$(".button").on("click", function() {
+$(document.body).on("click", ".button", function() {
     event.preventDefault();
     $(".gifs").empty();
     console.log($(this).attr("animal"));
     
-    // var buttonValue = $(this).text
     var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + $(this).attr("animal")  ;
     
     for (i=0 ; i<10; i++) { 
@@ -34,6 +33,18 @@ $(".button").on("click", function() {
             $(".gifs").append(aniGif);
         })
     }
+    $(".buttons").empty();
+    createButtons();
+})
+
+$(document.body).on("click", "img", function() {
+    if ($(this).attr("data-state") === "still") {
+        $(this).attr("src", $(this).attr("animatedURL"));
+        $(this).attr("data-state", "animated")
+    } else {
+        $(this).attr("src", $(this).attr("stillURL"));
+        $(this).attr("data-state", "still");
+    }
 })
 
 
@@ -42,66 +53,23 @@ $(".button").on("click", function() {
 $("#select-animal").on("click", function(event) {
     event.preventDefault();
     var input = $("#animal-input").val().trim();
-    
-    
-    var aniGif;
-    var newButt = $("<button>");
-    newButt.text(input);
-    $(".gifs").empty();
-    $(".buttons").append(newButt);
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + input ;
-    
-    for (i=0 ; i<10; i++) {    
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
-            console.log(response);
-            aniGif = $('<img>');
-            aniGif.css("height:300px;width:300px;margin:auto;");
-            console.log(response.data.image_url);
-            aniGif.attr("src" , response.data.image_url);
-            aniGif.attr("alt", "test");
-            $(".gifs").append(aniGif);
-        })
-    }
-    
-    $("#remove").on("click", function() {
-        $(".gifs").empty();
-    })
-    
+    animals.push(input);
+    $(".buttons").empty();
+    createButtons();
+    $("#animal-input").val("");
+    console.log(animals);
 })
 
+
+
 function createButtons(){ 
+    $(".buttons").empty();
     for (i=0 ; i<animals.length ; i++) {
         butt = $("<button>");
         butt.text(animals[i]);
         butt.attr("animal",animals[i]);
         butt.addClass("button");
+        butt.css("margin","5px");
         $(".buttons").append(butt);
         }
     }  
-
-//   console.log(response);
-//   console.log(response.data[0].url)
-//   displayGif(response);
-
-
-// });
-//   for (var i = 0; i < results.length; i++) {
-    //     var gifDiv = $("<div>");
-    
-    //     var rating = results[i].rating;
-    
-    //     var p = $("<p>").text("Rating: " + rating);
-    
-    //     var personImage = $("<img>");
-    //     personImage.attr("src", results[i].images.fixed_height.url);
-
-    //     gifDiv.prepend(p);
-    //     gifDiv.prepend(personImage);
-
-    //     $(".gifs").prepend(gifDiv);
-    //   }
-    // });
-    //once ajax call complete...
